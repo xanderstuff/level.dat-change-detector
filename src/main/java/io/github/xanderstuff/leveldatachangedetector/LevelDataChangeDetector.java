d9e0494e6dd1d994a1580e05100d1c9bcfa2fc34
xanderstuff <68503800+xanderstuff@users.noreply.github.com>
2022-04-30 16:28:09 -0500

    Auto format

--- src/main/java/io/github/xanderstuff/leveldatachangedetector/LevelDataChangeDetector.java
+++ src/main/java/io/github/xanderstuff/leveldatachangedetector/LevelDataChangeDetector.java
@@ -6,22 +6,22 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class LevelDataChangeDetector implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("LevelDataChangeDetector");
+    // This logger is used to write text to the console and the log file.
+    // It is considered best practice to use your mod id as the logger's name.
+    // That way, it's clear which mod wrote info, warnings, and errors.
+    public static final Logger LOGGER = LoggerFactory.getLogger("LevelDataChangeDetector");
     private long oldSeed = -69420; // magic! I love magic!
 
-	@Override
-	public void onInitialize() {
+    @Override
+    public void onInitialize() {
 //		LOGGER.info("Hello Fabric world!");
 
         ServerTickEvents.START_SERVER_TICK.register(server -> {
             long newSeed = server.getSaveProperties().getGeneratorOptions().getSeed();
-            if(oldSeed == -69420){
+            if (oldSeed == -69420) {
                 oldSeed = newSeed;
-            } else{
-                if(oldSeed != newSeed){
+            } else {
+                if (oldSeed != newSeed) {
                     LOGGER.error("ERROR!!!: the seed has been changed from: {}   to: {}", oldSeed, newSeed);
                 }
             }
