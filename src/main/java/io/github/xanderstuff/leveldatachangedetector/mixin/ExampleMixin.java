113e3bc9fb584fe28f16b5fb358f3cadc86bb96e
xanderstuff <68503800+xanderstuff@users.noreply.github.com>
2022-04-30 14:42:47 -0500

    Set mod id and basic metadata

--- src/main/java/io/github/xanderstuff/leveldatachangedetector/mixin/ExampleMixin.java
+++ src/main/java/io/github/xanderstuff/leveldatachangedetector/mixin/ExampleMixin.java
@@ -1,6 +1,6 @@
-package net.fabricmc.example.mixin;
+package io.github.xanderstuff.leveldatachangedetector.mixin;
 
-import net.fabricmc.example.ExampleMod;
+import io.github.xanderstuff.leveldatachangedetector.LevelDataChangeDetector;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		LevelDataChangeDetector.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
