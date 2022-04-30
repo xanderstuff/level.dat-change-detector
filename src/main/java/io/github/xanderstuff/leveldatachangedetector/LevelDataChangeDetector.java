fec79fa85ef80c8b8c78aa2bcd73852d38b565f4
xanderstuff <68503800+xanderstuff@users.noreply.github.com>
2022-04-30 16:25:59 -0500

    Track seed value

--- src/main/java/io/github/xanderstuff/leveldatachangedetector/LevelDataChangeDetector.java
+++ src/main/java/io/github/xanderstuff/leveldatachangedetector/LevelDataChangeDetector.java
@@ -1,6 +1,7 @@
 package io.github.xanderstuff.leveldatachangedetector;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,13 +10,34 @@ public class LevelDataChangeDetector implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("LevelDataChangeDetector");
+    private long oldSeed = -69420; // magic! I love magic!
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
 //		LOGGER.info("Hello Fabric world!");
-	}
+
+        ServerTickEvents.START_SERVER_TICK.register(server -> {
+            long newSeed = server.getSaveProperties().getGeneratorOptions().getSeed();
+            if(oldSeed == -69420){
+                oldSeed = newSeed;
+            } else{
+                if(oldSeed != newSeed){
+                    LOGGER.error("ERROR!!!: the seed has been changed from: {}   to: {}", oldSeed, newSeed);
+                }
+            }
+
+//            server.getWorlds().forEach(serverWorld -> {
+//
+//            });
+
+
+//            server.getSaveProperties().getMainWorldProperties().
+
+
+//            server.session.directory.toFile();
+            // get level.dat
+//            NbtIo.scanCompressed(file, new NbtCollector());
+//            var nbt = exclusiveNbtCollector.getRoot();
+        });
+    }
 }
